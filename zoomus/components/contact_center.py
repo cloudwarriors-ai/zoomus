48842e624f10be9c7bba110753a69d7d1228b95e
Doug <doug@mimicit.com>
2024-01-19 16:35:20 -0800

    contact center full automation

--- zoomus/components/contact_center.py
+++ zoomus/components/contact_center.py
@@ -5,6 +5,10 @@ from __future__ import absolute_import
 from zoomus import util
 from zoomus.components import base
 
+#add logging to the component
+
+import logging
+log = logging.getLogger(__name__)
 
 class ContactCenterComponentV2(base.BaseComponent):
     
@@ -28,6 +32,43 @@ class ContactCenterComponentV2(base.BaseComponent):
         print(kwargs)
         
         return self.post_request("/contact_center/queues/", data=kwargs)
+    
+    def queues_operating_hours_update(self, **kwargs):
+        util.require_keys(kwargs, "queue_id")
+        print("updating contact center operating hours",kwargs)
+        log.debug("updating contact center operating hours")
+
+        return self.patch_request("/contact_center/queues/{}/operating_hours".format(kwargs.get("queue_id")), data=kwargs)
+    
+    #/contact_center/business_hours
+
+    def queues_dispositions_update(self, **kwargs):
+        util.require_keys(kwargs, "queue_id")
+        print("updating contact center disposition",kwargs)
+        log.debug("updating contact center disposition")
+
+        return self.post_request("/contact_center/queues/{}/dispositions".format(kwargs.get("queue_id")), data=kwargs)
+    
+    def queues_disposition_set_update(self, **kwargs):
+        util.require_keys(kwargs, "queue_id")
+        print("updating contact center disposition",kwargs)
+        log.debug("updating contact center disposition")
+
+        return self.post_request("/contact_center/queues/{}/dispositions/sets".format(kwargs.get("queue_id")), data=kwargs)
+    
+
+    def business_hours(self, **kwargs):
+        return self.get_request("/contact_center/business_hours")
+    
+    def dispositions_list(self, **kwargs):
+        return self.get_request("/contact_center/dispositions")
+    
+    def dispositions_set_list(self, **kwargs):
+        return self.get_request("/contact_center/dispositions/sets")
+    
+
+
+    
     
     
     
